7dcc9586af9d68e7b3493a6038f0b39d13160382
Cynnar <jacob0305@gmail.com>
2020-08-20 04:24:12 +0000

    initial commit

---
+++ DialogLogParser.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DialogLogParser
+{
+    partial class DialogLogParser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogPath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lbNPCS = new System.Windows.Forms.ListBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLogPath
+            // 
+            this.txtLogPath.Location = new System.Drawing.Point(12, 32);
+            this.txtLogPath.Name = "txtLogPath";
+            this.txtLogPath.Size = new System.Drawing.Size(765, 20);
+            this.txtLogPath.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(783, 30);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(26, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lbNPCS
+            // 
+            this.lbNPCS.FormattingEnabled = true;
+            this.lbNPCS.Location = new System.Drawing.Point(12, 58);
+            this.lbNPCS.Name = "lbNPCS";
+            this.lbNPCS.Size = new System.Drawing.Size(120, 212);
+            this.lbNPCS.TabIndex = 2;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(344, 245);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(96, 23);
+            this.btnParse.TabIndex = 3;
+            this.btnParse.Text = "Generate Lua";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Log Path:";
+            // 
+            // DialogLogParser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 280);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.lbNPCS);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtLogPath);
+            this.Name = "DialogLogParser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dialog Log Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogPath;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ListBox lbNPCS;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DialogLogParser.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Diagnostics;
+
+namespace DialogLogParser
+{
+    public partial class DialogLogParser : Form
+    {
+        public DialogLogParser()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, NPC> NPCList = new Dictionary<string, NPC>();
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            // Current branch we are in so we know what to add options to
+            Branch current_branch = null;
+            NPC current_npc = null;
+            OpenFileDialog fd = new OpenFileDialog();
+            int index = 0;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                txtLogPath.Text = fd.FileName;
+                NPCList.Clear();
+                lbNPCS.Items.Clear();
+                lbNPCS.SelectedIndex = -1;
+
+                StreamReader reader = new StreamReader(txtLogPath.Text);
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line.Contains("You say, \"Hail,"))
+                    {
+                        index = line.IndexOf("Hail,") + 6;
+                        string npc_name = line.Substring(index, (line.Length - index) - 1);
+
+                        if (NPCList.ContainsKey(npc_name))
+                        {
+                            current_npc = NPCList[npc_name];
+                            current_branch = null;
+                        }
+                        else
+                        {
+                            NPC npc = new NPC();
+                            npc.Name = npc_name;
+                            NPCList.Add(npc_name, npc);
+                            current_npc = npc;
+                            current_branch = null;
+                            lbNPCS.Items.Add(npc_name);
+                        }
+                    }
+
+                    if (line.Contains("\\/a says to you"))
+                    {
+                        index = line.IndexOf(",") + 3; //line.IndexOf("says to you,") + 14;
+                        string reply = line.Substring(index, (line.Length - index) - 1);
+
+                        if (current_branch == null)
+                        {
+                            if (current_npc.Dialogs.Count > 0)
+                            {
+                                for (int i = 0; i < current_npc.Dialogs.Count; i++)
+                                {
+                                    if (current_npc.Dialogs[i].Text == reply)
+                                    {
+                                        current_branch = current_npc.Dialogs[i];
+                                        break;
+                                    }
+                                }
+                                if (current_branch == null)
+                                {
+                                    Branch branch = new Branch();
+                                    current_npc.Dialogs.Add(branch);
+                                    current_branch = branch;
+                                    current_branch.Text = reply;
+                                }
+
+                            }
+                            else
+                            {
+                                Branch branch = new Branch();
+                                current_npc.Dialogs.Add(branch);
+                                current_branch = branch;
+                                current_branch.Text = reply;
+                            }
+                        }
+                        else
+                        {
+                            if (string.IsNullOrEmpty(current_branch.Text))
+                            {
+                                current_branch.Text = reply;
+                            }
+                        }
+                    }
+
+                    // Tells to the guild can interfere as well as talking to yourself, not sure how to handle talking to yourself yet
+                    if (line.Contains("You say to ") && !line.Contains("You say to the guild"))
+                    {
+                        index = line.IndexOf(",") + 3;
+                        string reply = line.Substring(index, (line.Length - index) - 1);
+                        if (current_branch == null)
+                        {
+                            MessageBox.Show("Reply was orphaned:\n" + reply + "\n\nOriginal line from the log:\n" + line, "Error!", MessageBoxButtons.OK);
+                        }
+                        else
+                        {
+                            if (current_branch.Options.ContainsKey(reply))
+                                current_branch = current_branch.Options[reply];
+                            else
+                            {
+                                current_branch.Options.Add(reply, new Branch());
+                                current_branch = current_branch.Options[reply];
+                            }
+                        }
+                    }
+                }
+
+                reader.Close();
+            }
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            if (lbNPCS.SelectedIndex < 0)
+            {
+                return;
+            }
+
+            NPC npc = NPCList[lbNPCS.SelectedItem.ToString()];
+            npc.GenerateLua();
+        }
+    }
+
+    public class Branch
+    {
+        // What the NPC says to you
+        public string Text;
+        // What you say to the NPC and the new Branch it leads to
+        public Dictionary<string, Branch> Options;
+
+
+        public UInt32 optionID;
+
+        public Branch()
+        {
+            Options = new Dictionary<string, Branch>();
+            optionID = 0;
+        }
+    }
+
+    public class NPC
+    {
+        public string Name;
+        public List<Branch> Dialogs;
+
+        UInt32 option = 0;
+        public NPC()
+        {
+            Dialogs = new List<Branch>();
+        }
+
+        public void GenerateLua()
+        {
+            string file = Name.Replace(" ", "") + ".lua";
+            StreamWriter writer = new StreamWriter(Application.StartupPath + "/" + file);
+
+            writer.WriteLine("function hailed(NPC, Spawn)");
+            writer.WriteLine("\tFaceTarget(NPC, Spawn)");
+            writer.WriteLine("\tlocal conversation = CreateConversation()");
+
+            foreach(Branch b in Dialogs)
+            {
+                writer.WriteLine();
+                foreach (KeyValuePair<string, Branch> kvp in b.Options)
+                {
+                    if (kvp.Value == null || string.IsNullOrEmpty(kvp.Value.Text))
+                    {
+                        writer.WriteLine("\tAddConversationOption(conversation, \"" + kvp.Key + "\")");
+                    }
+                    else
+                    {
+                        writer.WriteLine("\tAddConversationOption(conversation, \"" + kvp.Key + "\", \"Option" + (kvp.Value.optionID = ++option) + "\")");
+                    }
+                }
+
+                writer.WriteLine("\tStartConversation(conversation, NPC, Spawn, \"" + b.Text +"\")");
+            }
+
+            writer.WriteLine("end");
+
+            foreach(Branch b in Dialogs)
+            {
+                foreach(KeyValuePair<string, Branch> kvp in b.Options)
+                {
+                    if (!string.IsNullOrEmpty(kvp.Value.Text))
+                    LuaAddFunctions(writer, kvp.Value);
+                }
+            }
+
+            writer.Close();
+
+            if (MessageBox.Show(file + "has been created, would you like to open it?", "Done!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                ProcessStartInfo info = new ProcessStartInfo(file);
+                Process.Start(info);
+            }
+        }
+
+        private void LuaAddFunctions(StreamWriter writer, Branch branch)
+        {
+            if (branch.optionID == 0)
+                return;
+
+            writer.WriteLine();
+            writer.WriteLine("function Option" + branch.optionID + "(NPC, Spawn)");
+            writer.WriteLine("\tFaceTarget(NPC, Spawn)");
+            writer.WriteLine("\tlocal conversation = CreateConversation()");
+            writer.WriteLine();
+
+            foreach (KeyValuePair<string, Branch> kvp in branch.Options)
+            {
+                writer.WriteLine("\tAddConversationOption(conversation, \"" + kvp.Key + "\"" + ((kvp.Value == null || string.IsNullOrEmpty(kvp.Value.Text)) ? ")" : (", \"Option" + (kvp.Value.optionID = ++option) + "\")")));
+                kvp.Value.optionID = option;
+            }
+
+            writer.WriteLine("\tStartConversation(conversation, NPC, Spawn, \"" + branch.Text + "\")");
+            writer.WriteLine("end");
+
+            foreach (KeyValuePair<string, Branch> kvp in branch.Options)
+            {
+                if (!string.IsNullOrEmpty(kvp.Value.Text))
+                    LuaAddFunctions(writer, kvp.Value);
+            }
+        }
+    }
+}
